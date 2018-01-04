45fd94a71a466bdefc9fa84982ea6f5595125575
shelvadim <shelgachev@yahoo.com>
2018-01-03 19:57:44 -0800

    1

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-//git remote set-url origin https://github.com/Shelvadim/React-Route-Template.git
-
+//git remote set-url origin https://github.com/Shelvadim/portfolio.git
 
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
